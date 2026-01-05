822fa679b3f37e1d7d3332f08636bed00b36508d
cash <cashwugeek@icloud.com>
2026-01-05 19:38:10 +0800

    Use H.264 codec by default for better macOS compatibility
    Add [vcodec^=avc] filter to default download command to force H.264
    encoding instead of AV1. This ensures downloaded videos can be:
    - Previewed with Quick Look (spacebar)
    - Display thumbnails in Finder and other apps

--- Tubify/Models/AppSettings.swift
+++ Tubify/Models/AppSettings.swift
@@ -10,7 +10,7 @@ enum AppSettingsKeys {
 /// App 預設設定值
 enum AppSettingsDefaults {
     // 使用 Safari cookies 下載（需要完整磁碟存取權限）
-    static let downloadCommand = "yt-dlp -f \"bv[ext=mp4]+ba[ext=m4a]/b[ext=mp4]\" --cookies-from-browser safari \"$youtubeUrl\""
+    static let downloadCommand = "yt-dlp -f \"bv[ext=mp4][vcodec^=avc]+ba[ext=m4a]/b[ext=mp4][vcodec^=avc]\" --cookies-from-browser safari \"$youtubeUrl\""
     static let downloadFolder = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).first?.path ?? "~/Downloads"
     static let maxConcurrentDownloads: Int = 2 // 最大同時下載數量（1-5）
 }
